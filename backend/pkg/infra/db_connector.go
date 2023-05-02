4de54319fe771c8f53a17d410c7af0a2b14046db
Tufusa <louptung@gmail.com>
2023-05-02 11:31:42 +0900

    feat: DBとの接続

---
+++ backend/pkg/infra/db_connector.go
@@ -0,0 +1,40 @@
+package infra
+
+import (
+	"fmt"
+
+	"github.com/tufusa/article-api/backend/pkg/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBConnector struct {
+	Conn *gorm.DB
+}
+
+func NewDBConnector() *DBConnector {
+	conf := config.LoadConfig()
+	dsn := generateDSN(*conf.DB)
+
+	conn, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect database")
+	}
+
+	return &DBConnector{
+		Conn: conn,
+	}
+}
+
+func generateDSN(conf config.DBConfig) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&loc=Local",
+		conf.Username,
+		conf.Password,
+		conf.Addr,
+		conf.DBName,
+	)
+}
