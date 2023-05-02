ec97f90c92862e964b330e1485f576f73a6ffc45
Tufusa <louptung@gmail.com>
2023-05-02 11:34:08 +0900

    feat: GETテストを追加

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,25 @@
-export const Home = () => (
-  <div className="m-1">
-    Home page.
-  </div>
-)
+import { useState } from "react"
+import { client } from "../libs/axios"
+
+export const Home = () => {
+  const [response, setResponse] = useState("")
+  const get = async () => {
+    client.get("/articles").then((response) => {
+      setResponse(response.data)
+    })
+  }
+
+  return (
+    <div className="mx-4 my-2">
+      <div className="text-3xl">Home page.</div>
+      <hr className="my-2" />
+      <button onClick={get} className="border border-black px-1">
+        Get
+      </button>
+      <div>
+        Response:<br />
+        {response}
+      </div>
+    </div>
+  )
+}
